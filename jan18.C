5cd724641d962de0406b08dcc50c2d4d57ec6531
TARANPREET KAUR <119510143+tanuravu@users.noreply.github.com>
2023-03-05 13:47:20 +0530

    Add files via upload

---
+++ jan18.C
@@ -0,0 +1,71 @@
+/*
+//write a c program to  perform bitwise operations
+#include<stdio.h>
+
+int main(){
+    int a,b;
+    a=5;
+    b=6;
+    printf("\n%d",a&b);
+    printf("\n%d",a|b);
+    printf("\n%d",a^b);
+    return 0;
+}*/
+/*
+// write a c program the operator precedence on expression:
+#include<stdio.h>
+int main() {
+    int a,b,c,d,x;
+    printf("enter the value of a,b,c,d:");
+    scanf("%d%d%d%d",&a,&b,&c,&d);
+    x=a+b*c/d;
+    printf("%d",x);
+    return 0;
+}
+
+
+#include<stdio.h>
+int main() {
+    int a;
+    a=(1,2,3);
+    printf("%d",a);
+    return 0;
+}*/
+
+/*
+#include<stdio.h>
+int main() {
+    int h1,h2;
+    h1=20;
+    h2=40;
+    if(h1>h2)
+    {
+    printf(" h1 is taller");
+    }
+    if (h2>h1);
+    {
+    printf("h2 is taller");
+    }
+    return 0;
+}
+*/
+/*
+
+//blood donation:
+#include<stdio.h>
+int main() {
+    int age,weight;
+    printf("ENTER THE AGE AND WEIGHT OF A PERSON");
+    scanf("%d%d",&age,&weight);
+    if(age>18 && weight>50)
+    {
+        printf("person is capable");
+    }
+    else
+    {
+        printf("person is not capable");
+    }
+    return 0;
+}
+*/
+
